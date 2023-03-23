7605f0dd6b4ea2909917f88a1be4c81b9be78a35
Kacper Paszek <105833652+Kelposs@users.noreply.github.com>
2023-03-23 16:24:13 +0900

    Asset追加

--- lib/gen/assets.gen.dart
+++ lib/gen/assets.gen.dart
@@ -8,6 +8,14 @@
 // ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
 
 import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter/services.dart';
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  $AssetsImagesIconGen get icon => const $AssetsImagesIconGen();
+}
 
 class $AssetsTranslationsGen {
   const $AssetsTranslationsGen();
@@ -19,9 +27,73 @@ class $AssetsTranslationsGen {
   List<String> get values => [jaJP];
 }
 
+class $AssetsImagesIconGen {
+  const $AssetsImagesIconGen();
+
+  /// File path: assets/images/icon/icon_fork_dark.svg
+  SvgGenImage get iconForkDark =>
+      const SvgGenImage('assets/images/icon/icon_fork_dark.svg');
+
+  /// File path: assets/images/icon/icon_fork_light.svg
+  SvgGenImage get iconForkLight =>
+      const SvgGenImage('assets/images/icon/icon_fork_light.svg');
+
+  /// File path: assets/images/icon/icon_github_dark.svg
+  SvgGenImage get iconGithubDark =>
+      const SvgGenImage('assets/images/icon/icon_github_dark.svg');
+
+  /// File path: assets/images/icon/icon_github_grey.svg
+  SvgGenImage get iconGithubGrey =>
+      const SvgGenImage('assets/images/icon/icon_github_grey.svg');
+
+  /// File path: assets/images/icon/icon_github_light.svg
+  SvgGenImage get iconGithubLight =>
+      const SvgGenImage('assets/images/icon/icon_github_light.svg');
+
+  /// File path: assets/images/icon/icon_issues_dark.svg
+  SvgGenImage get iconIssuesDark =>
+      const SvgGenImage('assets/images/icon/icon_issues_dark.svg');
+
+  /// File path: assets/images/icon/icon_issues_light.svg
+  SvgGenImage get iconIssuesLight =>
+      const SvgGenImage('assets/images/icon/icon_issues_light.svg');
+
+  /// File path: assets/images/icon/icon_star_dark.svg
+  SvgGenImage get iconStarDark =>
+      const SvgGenImage('assets/images/icon/icon_star_dark.svg');
+
+  /// File path: assets/images/icon/icon_star_light.svg
+  SvgGenImage get iconStarLight =>
+      const SvgGenImage('assets/images/icon/icon_star_light.svg');
+
+  /// File path: assets/images/icon/icon_watchers_dark.svg
+  SvgGenImage get iconWatchersDark =>
+      const SvgGenImage('assets/images/icon/icon_watchers_dark.svg');
+
+  /// File path: assets/images/icon/icon_watchers_light.svg
+  SvgGenImage get iconWatchersLight =>
+      const SvgGenImage('assets/images/icon/icon_watchers_light.svg');
+
+  /// List of all assets
+  List<SvgGenImage> get values => [
+        iconForkDark,
+        iconForkLight,
+        iconGithubDark,
+        iconGithubGrey,
+        iconGithubLight,
+        iconIssuesDark,
+        iconIssuesLight,
+        iconStarDark,
+        iconStarLight,
+        iconWatchersDark,
+        iconWatchersLight
+      ];
+}
+
 class Assets {
   Assets._();
 
+  static const $AssetsImagesGen images = $AssetsImagesGen();
   static const $AssetsTranslationsGen translations = $AssetsTranslationsGen();
 }
 
@@ -84,6 +156,59 @@ class AssetGenImage {
   }
 
   ImageProvider provider() => AssetImage(_assetName);
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
+
+class SvgGenImage {
+  const SvgGenImage(this._assetName);
+
+  final String _assetName;
+
+  SvgPicture svg({
+    Key? key,
+    bool matchTextDirection = false,
+    AssetBundle? bundle,
+    String? package,
+    double? width,
+    double? height,
+    BoxFit fit = BoxFit.contain,
+    AlignmentGeometry alignment = Alignment.center,
+    bool allowDrawingOutsideViewBox = false,
+    WidgetBuilder? placeholderBuilder,
+    String? semanticsLabel,
+    bool excludeFromSemantics = false,
+    SvgTheme theme = const SvgTheme(),
+    ColorFilter? colorFilter,
+    @deprecated Color? color,
+    @deprecated BlendMode colorBlendMode = BlendMode.srcIn,
+    @deprecated Clip? clipBehavior,
+    @deprecated bool cacheColorFilter = false,
+  }) {
+    return SvgPicture.asset(
+      _assetName,
+      key: key,
+      matchTextDirection: matchTextDirection,
+      bundle: bundle,
+      package: package,
+      width: width,
+      height: height,
+      fit: fit,
+      alignment: alignment,
+      allowDrawingOutsideViewBox: allowDrawingOutsideViewBox,
+      placeholderBuilder: placeholderBuilder,
+      semanticsLabel: semanticsLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      theme: theme,
+      colorFilter: colorFilter,
+      color: color,
+      colorBlendMode: colorBlendMode,
+      clipBehavior: clipBehavior,
+      cacheColorFilter: cacheColorFilter,
+    );
+  }
 
   String get path => _assetName;
 
